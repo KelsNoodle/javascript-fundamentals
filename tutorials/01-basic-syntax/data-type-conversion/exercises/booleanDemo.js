2b29952e0479592757024ab77fa99cec9bffa62e
Kelsea W <kelsea.wessel@google.com>
2026-05-22 11:46:30 -0500

    Completed exercises

--- tutorials/01-basic-syntax/data-type-conversion/exercises/booleanDemo.js
+++ tutorials/01-basic-syntax/data-type-conversion/exercises/booleanDemo.js
@@ -8,6 +8,14 @@ let alienLifeDetected = null;
 let darkMatter = undefined;
 let imaginaryNumber = NaN;
 
+humanVisitorsToMars = false;
+screamInSpace = false;
+alienLifeDetected = false;
+darkMatter = false;
+imaginaryNumber = false;
+
+console.log(humanVisitorsToMars, screamInSpace, alienLifeDetected, darkMatter, imaginaryNumber);
+
 // DEMO: Convert each value above to Boolean and log its new value.
 
 /** EXAMPLE 2 - TRUTHY VALUES */
@@ -16,6 +24,11 @@ let humanVisitorsToMoon = 28;
 let firstHumanToWalkOnMoon = "Neil Armstrong";
 let buzzLightyearDestination = Infinity;
 
+humanVisitorsToMoon = true;
+firstHumanToWalkOnMoon = true;
+buzzLightyearDestination = true;
+
+console.log(humanVisitorsToMoon, firstHumanToWalkOnMoon, buzzLightyearDestination);
 // DEMO: Convert each value above to Boolean and log its new value.
 
 // DEMO: Make a git commit!
