110f61c786ddd8bc4ea09b2a0b6703014cfbb78c
Kelsea W <kelsea.wessel@google.com>
2026-05-22 10:17:46 -0500

    Completed demo exercise

--- tutorials/01-basic-syntax/code-execution/exercises/demo.js
+++ tutorials/01-basic-syntax/code-execution/exercises/demo.js
@@ -6,17 +6,28 @@
 
 // DEMO: Re-order the lines below so that there are no reference errors
 // and the total prints as expected.
-console.log(total);
-let total = num1 + num2;
+
+
 let num1 = 3;
 let num2 = 4;
-
+let total = num1 + num2;
+console.log(`The sum of ${num1} and ${num2} is:${total}`);
 /** EXAMPLE 2 - REASSIGNMENT **/
 
 // DEMO: Reassign the value of num multiple times, and use console.log statements.
 // to see those changes reflected at different points along the way.
 
 let num = 1;
+num = num + 3;
+console.log(num);
+num = num + 3;
+console.log(num);
+num = num + 3;
+console.log(num);
+num = num + 3;
+console.log(num);
+num = num + 3;
+console.log(num);
 
 // DEMO: Make a git commit!
 
